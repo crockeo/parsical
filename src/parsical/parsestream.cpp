8b71eb03e354352361f3a45a5d666cbde0f8f857
Cerek Hillen <crockeo@gmail.com>
2015-05-01 09:13:05 -0700

    Implemented a StringParser. I need to write some test cases.

--- src/parsical/parsestream.cpp
+++ src/parsical/parsestream.cpp
@@ -1,1 +1,35 @@
 #include "parsestream.hpp"
+
+// Constructing a StringParser from a given string.
+parsical::StringParser::StringParser(std::string str) :
+        str(str),
+        p(0) { }
+
+// Checking whether this ParseStream has reached its end.
+bool parsical::StringParser::eof() const noexcept {
+    return pos() >= str.size();
+}
+
+// Peeking at the next value without consuming it.
+char parsical::StringParser::peek() const throw(parsical::ParseError) {
+    if (eof())
+        throw ParseError("Cannot peek after EOF has been reached.");
+    return str.at(pos());
+}
+
+// Getting the current position in this ParseStream.
+int parsical::StringParser::pos() const noexcept { return p; }
+
+// Consuming and returning a value.
+char parsical::StringParser::get() throw(parsical::ParseError) {
+    if (eof())
+        throw ParseError("Cannot get after EOF has been reached.");
+    return str.at(p++);
+}
+
+// Stepping back some interval.
+void parsical::StringParser::stepBack(int n) throw(parsical::ParseError) {
+    if (n > pos())
+        throw ParseError("Stepping back so far would make the current position negative.");
+    p -= n;
+}
--- src/parsical/parsestream.hpp
+++ src/parsical/parsestream.hpp
@@ -1,10 +1,79 @@
+// Name: parsical/parsestream.hpp
+//
+// Description:
+//   This contains a generic parse stream API with a couple of implementations
+//   for different situations.
+
 #ifndef _PARSICAL_PARSER_STREAM_HPP_
 #define _PARSICAL_PARSER_STREAM_HPP_
+
+//////////////
+// Includes //
+#include <istream>
+#include <string>
+
+#include "parseerror.hpp"
 
 //////////
 // Code //
 
-// An input stream to perform the rest of the parsing upon.
-class ParseStream { };
+namespace parsical {
+    // The generic ParseStream interface.
+    template <typename T>
+    struct ParseStream {
+        // Virtual destructor to preemptively eliminate any problems with
+        // inherited deconstruction.
+        virtual ~ParseStream() { }
+
+        // Checking whether this ParseStream has reached its end.
+        virtual bool eof() const noexcept = 0;
+
+        // Peeking at the next value without consuming it.
+        virtual T peek() const throw(ParseError) = 0;
+
+        // Getting the current position in this ParseStream.
+        virtual int pos() const noexcept = 0;
+
+        // Consuming and returning a value.
+        virtual T get() throw(ParseError) = 0;
+
+        // Stepping back some interval.
+        virtual void stepBack(int) throw(ParseError) = 0;
+
+        // Un-getting a single character. It ought to be equivalent to
+        // stepBack(1)
+        virtual void unget() throw(ParseError) { stepBack(1); }
+    };
+
+    // A parser specifically desinged around parsing a string.
+    class StringParser : public ParseStream<char> {
+    private:
+        std::string str;
+        int p;
+
+    public:
+        // Constructing a StringParser from a given string.
+        StringParser(std::string);
+
+        // Checking whether this ParseStream has reached its end.
+        virtual bool eof() const noexcept override;
+
+        // Peeking at the next value without consuming it.
+        virtual char peek() const throw(ParseError) override;
+
+        // Getting the current position in this ParseStream.
+        virtual int pos() const noexcept override;
+
+        // Consuming and returning a value.
+        virtual char get() throw(ParseError) override;
+
+        // Stepping back some interval.
+        virtual void stepBack(int) throw(ParseError) override;
+
+        // Un-getting a single character. It ought to be equivalent to
+        // stepBack(1)
+        virtual void unget() throw(ParseError) override;
+    };
+}
 
 #endif
