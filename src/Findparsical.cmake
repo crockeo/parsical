2a9e8af499b081f6f7d1535d7812dfc197f13f18
Cerek Hillen <crockeo@gmail.com>
2015-05-04 21:52:35 -0700

    Added a Findparsical file. I need to test if it's actually working.

---
+++ src/Findparsical.cmake
@@ -0,0 +1,28 @@
+# Name: Findparsical.cmake
+#
+# Description:
+#   A cmake file allowing an easier time to find and use this library when using
+#   CMake.
+
+SET(SEARCH_DIRECTORIES
+  /usr/local
+)
+
+# Finding the parsical headers.
+find_path(PARSICAL_INCLUDE_DIR parsical.hpp
+  PATH_SUFFIX include
+  PATHS ${SEARCH_DIRECTORIES}
+)
+
+# Finding the parsical library.
+find_library(PARSICAL_LIBRARY
+  NAMES parsical
+  PATH_SUFFIXES lib lib/static
+  PATHS ${SERACH_DIRECTORIES}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  CLIBGAME
+  REQUIRED_VARS CLIBGAME_INCLUDE_DIR CLIBGAME_LIBRARY
+)
